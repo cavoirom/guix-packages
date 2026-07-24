5bfae03a119df02dd22b9e67ec718a4971866a9a
Cavoi Rom <cavoirom.wanted@yahoo.com>
2026-07-25 01:00:19 +0700

    update ampcode to version 0.0.1784938774-g22b3fc.

--- private/packages/ampcode.scm
+++ private/packages/ampcode.scm
@@ -7,7 +7,7 @@
 
 ;; Get new version: curl -s https://static.ampcode.com/cli/cli-version.txt
 (define ampcode-version
-  "0.0.1784782915-g1b24ab")
+  "0.0.1784938774-g22b3fc")
 
 (define ampcode-platform
   "linux-arm64")
@@ -25,7 +25,7 @@
        ;; Update version and hash together; never fetch a moving latest binary.
        ;; Get new hash: guix download https://static.ampcode.com/cli/$(curl -s https://static.ampcode.com/cli/cli-version.txt)/amp-linux-arm64
        (sha256
-        (base32 "0s4g5m8rm133f96pfzyz7pzldw85wv3ir4p6wsjyf44ab6g5xmal"))))
+        (base32 "1xwakzf63pn7yw6fg6v0wmby1nl2vbgisjbxp4idlaj5gljy2kjz"))))
     (build-system copy-build-system)
     ;; This upstream binary is built for conventional Linux/FHS and is expected
     ;; to run through `guix shell --emulate-fhs`.
